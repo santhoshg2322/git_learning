decc1f7d9b4434dbe418da2770f14e4c59e994cd
santhoshg2322 <99637208+santhoshg2322@users.noreply.github.com>
2022-02-15 06:52:02 +0530

    update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
+using namespace std;
 int main(int argc, char *argv[])
 {
     int a,b,res;
     a=10;
     b=20;
     res=a+b;
-   std::cout<<res;
+    cout<<res;
     return 0;
 }
